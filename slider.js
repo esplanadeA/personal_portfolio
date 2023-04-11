118a153614e859d3b6c7ea08d350c2359c1c7374
esplanadeA <anna.li.dev@gmail.com>
2023-04-11 19:03:14 +1200

    contact & information js added

--- slider.js
+++ slider.js
@@ -14,3 +14,29 @@ projectContainer.forEach((item, i) => {
     item.scrollLeft -= containerWidth
   })
 })
+
+$(document).ready(function () {
+  $('form').submit(function (e) {
+    e.preventDefault() // prevent the form from submitting
+
+    var name = $('#name').val()
+    var email = $('#email').val()
+    var feedback = $('#feedback').val()
+
+    // validate the form data
+    if (name.length == 0 || email.length == 0 || feedback.length == 0) {
+      alert('Please fill out all fields.')
+      return
+    }
+
+    // send the form data to the server using AJAX
+    $.ajax({
+      type: 'POST',
+      url: '/submit-feedback',
+      data: {
+        name: name,
+        email,
+      },
+    })
+  })
+})
